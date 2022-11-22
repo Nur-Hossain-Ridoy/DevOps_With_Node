70face08ed433869fac2c0967093f1915eeb55aa
NH Ridoy <ridoydeveloper@gmail.com>
2022-11-22 15:46:16 +0600

    two container talk each other

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const {
+    MONGO_IP,
+    MONGO_PASSWORD,
+    MONGO_PORT,
+    MONGO_USER
+} = require('./config/config')
 
 const app = express()
+
+mongoose
+    .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    .then(() => {
+        console.log("Database Connected")
+    })
+    .catch(err => console.log(err))
+
 
 app.get('/', (req, res) => {
     res.send('<h2>A Quite Place</h2>')
